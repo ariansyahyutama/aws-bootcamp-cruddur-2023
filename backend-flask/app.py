be333e2ae0810945251211808db088a2643fc864
Ariansyah Yutama <ariansyahy@yahoo.com>
2023-03-04 05:02:43 +0000

    add xray

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -11,6 +11,12 @@ from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExport
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
+# xray ---
+
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+
 from services.home_activities import *
 from services.notifications_activities import *
 from services.user_activities import *
@@ -28,6 +34,11 @@ processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
+
+# xray
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
 
 app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
