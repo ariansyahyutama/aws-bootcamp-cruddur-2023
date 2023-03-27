f20203854b6ba4caa6b3d9a5ba6ceb4bc98f88c9
Ariansyah Yutama <ariansyahy@yahoo.com>
2023-03-27 08:56:18 +0000

    complete the app - postgres conn

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
-from lib.db import pool
+from lib.db import pool,query_wrap_array
 
 tracer = trace.get_tracer("home.activities")
 
@@ -8,28 +8,26 @@ class HomeActivities:
   def run(cognito_user_id=None):
       print("HOME ACTIVITY")
       #logger.info("HomeActivities")
-      with tracer.start_as_current_span("home-activites-mock-data"):
-        span = trace.get_current_span()
-        now = datetime.now(timezone.utc).astimezone()
-        span.set_attribute("app.now", now.isoformat())
+      #with tracer.start_as_current_span("home-activites-mock-data"):
+      #  span = trace.get_current_span()
+      #  now = datetime.now(timezone.utc).astimezone()
+      #  span.set_attribute("app.now", now.isoformat())
 
-        sql = """
-        SELECT * FROM activities
-        """
+      sql = query_wrap_array("""
+      SELECT * FROM activities
+      """)
 
-        with pool.connection() as conn:
-          with conn.cursor() as cur:
-            cur.execute(sql)
-            # this will return a tuple
-            # the first field being the data
-            #json = cur.fetchall()
-            rows = cur.fetchall()
+      with pool.connection() as conn:
+        with conn.cursor() as cur:
+          cur.execute(sql)
+          # this will return a tuple
+          # the first field being the data
+          json = cur.fetchone()
+          #rows = cur.fetchall()
 
-            for row in rows:
-              print(row)
-        
-        #print("----")
-        #print(json)
-        return json[0]
-        return results
+          #for row in rows:
+          #  print(row)
+      
+      return json[0]
+      return results
       
