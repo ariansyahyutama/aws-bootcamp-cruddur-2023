a08a375b0d2902c22bea8c801ca9c4f893742f2e
Ariansyah Yutama <ariansyahy@yahoo.com>
2023-04-14 23:12:29 +0000

    add lando

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,7 +3,8 @@ INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrewbrown' , 'test@email.com', 'MOCK'),
   ('ariansyah', 'ariansyahy' , 'ariansyahy@yahoo.com', 'MOCK'),
-  ('Andrew Bayko', 'bayko' , 'ghurafa2821@gmail.com' , 'MOCK');
+  ('Andrew Bayko', 'bayko' , 'ghurafa2821@gmail.com' , 'MOCK'),
+  ('lando', 'lando' , 'lando@gmail.com' , 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
