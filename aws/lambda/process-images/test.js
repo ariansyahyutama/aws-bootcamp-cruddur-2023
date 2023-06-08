a3ad53799e3fd1d30204313434a3bb2b0798214e
Ariansyah Yutama <ariansyahy@yahoo.com>
2023-06-08 10:31:46 +0000

    wip

--- aws/lambda/process-images/test.js
+++ aws/lambda/process-images/test.js
@@ -1,18 +1,18 @@
-const { getClient, getOriginalImage, processImage, uploadProcessedImage } = require('./s3-image-processing.js')
+const {getClient, getOriginalImage, processImage, uploadProcessedImage} = require('./s3-image-processing.js')
 
-async function main() {
-    client = getClient()
-    const srcBucket = 'assets.annleefores.cloud'
-    const srcKey = 'avatar/original/data.jpg'
-    const dstBucket = 'assets.annleefores.cloud'
-    const dstKey = 'avatar/processed/data.png'
-    const width = 256
-    const height = 256
+async function main(){
+  client = getClient()
+  const srcBucket = 'cruddur-thumbs'
+  const srcKey = 'avatar/original/data.jpg'
+  const dstBucket = 'cruddur-thumbs'
+  const dstKey = 'avatar/processed/data.png'
+  const width = 256
+  const height = 256
 
-    const originalImage = await getOriginalImage(client, srcBucket, srcKey)
-    console.log(originalImage)
-    const processedImage = await processImage(originalImage, width, height)
-    await uploadProcessedImage(client, dstBucket, dstKey, processedImage)
+  const originalImage = await getOriginalImage(client,srcBucket,srcKey)
+  console.log(originalImage)
+  const processedImage = await processImage(originalImage,width,height)
+  await uploadProcessedImage(client,dstBucket,dstKey,processedImage)
 }
 
 main()
