0163eaf1cb4cec0e231f97bbadc660e7ea095978
Ariansyah Yutama <ariansyahy@yahoo.com>
2023-07-05 09:59:30 +0000

    wip

--- frontend-react-js/src/pages/UserFeedPage.js
+++ frontend-react-js/src/pages/UserFeedPage.js
@@ -8,6 +8,7 @@ import ActivityFeed from '../components/ActivityFeed';
 import ActivityForm from '../components/ActivityForm';
 import EditProfileButton from '../components/EditProfileButton';
 import ProfileHeading from '../components/ProfileHeading';
+import ProfileForm from '../components/ProfileForm';
 
 // [TODO] Authenication
 import Cookies from 'js-cookie'
